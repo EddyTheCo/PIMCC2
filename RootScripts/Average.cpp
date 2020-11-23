a9c8ef4d4a861bc595482e2d035c907d108a46a0
EddyTheCo <eddyglezz95@gmail.com>
2020-11-23 16:07:25 +0100

    added the script to plot density maps

---
+++ RootScripts/Average.cpp
@@ -0,0 +1,154 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+TFile *MyFile = new TFile("RootFile.root","UPDATE");
+TVectorD *v=nullptr;
+
+
+void XZProj (TH1 * hist,string Title)
+{
+        gROOT->SetBatch(kTRUE);
+         TH1 * xzProj =nullptr;
+    if(hist->GetZaxis())
+    {
+        xzProj=((TH3D*)hist)->Project3D("xz");
+    }
+    else
+    {
+        if(hist->GetYaxis())
+        {
+            xzProj=(TH1*)hist->Clone();
+        }
+        else
+        {
+            return;
+
+        }
+    }
+
+
+
+    TCanvas*c1 = new TCanvas();
+    gStyle->SetOptStat(0);
+    xzProj->Draw("CONT4Z");
+    xzProj->GetXaxis()->SetTitle("z");
+    xzProj->GetYaxis()->CenterTitle(true);
+    xzProj->GetYaxis()->SetTitle("x");
+   xzProj->GetXaxis()->CenterTitle(true);
+    xzProj->SetTitle("");
+    c1->Print(("xzProj"+Title+".png").c_str());
+    return xzProj;
+}
+void XYProj (TH1 * hist, string Title)
+{
+        gROOT->SetBatch(kTRUE);
+    TH1 * xyProj =nullptr;
+
+    if(hist->GetZaxis())
+    {
+        xyProj=((TH3D*)hist)->Project3D("xy");
+    }
+    else
+    {
+        if(hist->GetYaxis())
+        {
+            xyProj=(TH1*)hist->Clone();
+        }
+        else
+        {
+            return;
+
+        }
+    }
+
+
+    TCanvas*c1 = new TCanvas();
+    gStyle->SetOptStat(0);
+    xyProj->Draw("CONT4Z");
+    xyProj->GetYaxis()->SetTitle("X");
+    xyProj->GetYaxis()->CenterTitle(true);
+    xyProj->GetXaxis()->SetTitle("Y");
+   xyProj->GetXaxis()->CenterTitle(true);
+    xyProj->SetTitle("");
+    c1->Print(("xyProj"+Title+".png").c_str());
+    return xyProj;
+}
+
+void Average ()
+{
+gROOT->SetBatch(kTRUE);
+        TH1 * Ave =nullptr;
+
+
+    size_t stp=0;
+
+
+    v = (TVectorD*)gDirectory->Get("v");
+    TH1* hist = nullptr;
+    bool var=1;
+
+    TCanvas*c1 = new TCanvas("c1", "c1", 1200,1000);
+    gStyle->SetOptStat(0);
+
+
+    for(int i=1;i<=(((*v)[0]<1000)?((*v)[0]):(999));i++)
+        {
+
+            cout<<("pos" + to_string(i)).c_str()<<endl;
+            hist=(TH1* )gDirectory->Get(("pos" + to_string(i)).c_str());
+            if(hist!=nullptr)
+            {
+
+                if(var)
+                {
+                    Ave=(TH1*)hist->Clone();
+
+                    var=0;
+
+                }
+                else
+                {
+
+                   Ave->Add(hist);
+
+
+                }
+
+            }
+            delete hist;
+            stp=i;
+
+        }
+
+    hist=(TH1* )gDirectory->Get(("pos" + to_string(stp)).c_str());
+    hist->GetXaxis()->SetTitle("X");
+    hist->GetYaxis()->SetTitle("Y");
+    hist->GetZaxis()->SetTitle("Z");
+    hist->GetYaxis()->CenterTitle(true);
+    hist->GetZaxis()->CenterTitle(true);
+    hist->GetXaxis()->CenterTitle(true);
+    hist->Draw();
+    c1->Print("AverageLast.png");
+
+    XYProj(hist,"Last");
+    XZProj(hist,"Last");
+    Ave->GetXaxis()->SetTitle("X");
+    Ave->GetYaxis()->SetTitle("Y");
+    Ave ->GetZaxis()->SetTitle("Z");
+    Ave->GetYaxis()->CenterTitle(true);
+    Ave->GetZaxis()->CenterTitle(true);
+    Ave->GetXaxis()->CenterTitle(true);
+    Ave->Draw();
+    c1->Print("Average.png");
+
+    XYProj(hist,"Average");
+    XZProj(hist,"Average");
+    Ave->Scale(1.0/stp);
+    Ave->Write("Average",TObject::kOverwrite);
+    gDirectory->Write("", TObject::kOverwrite);
+
+
+
+
+
+}
